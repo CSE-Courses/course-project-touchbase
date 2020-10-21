a5f33ac54db2255b7d2c7c77762b01aedfd484d4
Quinn Tarbell <qmtarbell@gmail.com>
2020-10-20 23:00:05 -0400

    Added basic UI functionality #29

--- packages/core/frontend/src/components/AddButton.vue
+++ packages/core/frontend/src/components/AddButton.vue
@@ -14,9 +14,22 @@
           <v-icon v-else> mdi-plus </v-icon>
         </v-btn>
       </template>
-      <v-btn fab dark small color="primary">
-        <v-icon>mdi-pencil</v-icon>
-      </v-btn>
+      <v-tooltip top>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            fab
+            dark
+            small
+            color="primary"
+            v-bind="attrs"
+            v-on="on"
+            @click="resourceDialog = !resourceDialog"
+          >
+            <v-icon>mdi-pencil</v-icon>
+          </v-btn>
+        </template>
+        <span>Add Resource</span>
+      </v-tooltip>
       <v-btn fab dark small color="primary" @click="dialog = !dialog">
         <v-icon>mdi-folder-open</v-icon>
       </v-btn>
@@ -25,6 +38,27 @@
       <v-card>
         <v-card-text>
           <v-text-field v-model="collectionName" label="Collection name" required></v-text-field>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn text color="primary" @click="submit"> Submit </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-dialog v-model="resourceDialog" max-width="500px">
+      <v-card>
+        <v-card-text>
+          <v-text-field v-model="resourceName" label="Resource name" required></v-text-field>
+          <v-select v-model="resourceType" label="Resource type" :items="resourceTypes"></v-select>
+          <v-text-field
+            v-for="arbit in resourceInputs[resourceType]"
+            :key="arbit.valueOf"
+            :input="arbit"
+            :label="arbit"
+          >
+          </v-text-field>
         </v-card-text>
 
         <v-card-actions>
@@ -51,6 +85,16 @@ export default class AddButton extends Vue {
   fab = false;
 
   dialog = false;
+
+  resourceName = "";
+
+  resourceDialog = false;
+
+  resourceTypes = ["Hyperlink"];
+
+  resourceType = null;
+
+  resourceInputs = { Hyperlink: ["URL"] };
 
   collectionName = "";
 
