9aed25f873e84a99c1390b76d38299b3f91a2947
Qmtfive <qmtarbell@gmail.com>
2020-12-01 14:01:09 -0500

    Create event system #99

--- packages/core/frontend/src/views/lists/CalendarList.vue
+++ packages/core/frontend/src/views/lists/CalendarList.vue
@@ -1,3 +1,53 @@
 <template>
-  <v-calendar color="background"></v-calendar>
+  <v-calendar color="background" :events="events" @click:event="openEvent"></v-calendar>
 </template>
+
+<script lang="ts">
+import { Vue, Component, Watch } from "vue-property-decorator";
+import api from "@/api";
+import EditResourceButton from "@/components/EditResourceButton.vue";
+import EditCollectionButton from "@/components/EditCollectionButton.vue";
+import router from "@/router";
+
+const resourceService = api.service("resources");
+@Component({
+  components: { EditCollectionButton, EditResourceButton },
+})
+export default class BrowseList extends Vue {
+  resources: { name: string; date: string; id: string }[] = [];
+
+  events: { name: string; start: string; id: string }[] = [];
+  // eslint-disable-next-line
+  openEvent(event: any): void {
+    router.push(`/app/resource/${event.event.id}`);
+  }
+
+  @Watch("$route")
+  async pullResources(): Promise<void> {
+    const authRes = await api.get("authentication");
+    this.resources = (
+      await resourceService.find({
+        query: {
+          ownerID: authRes.user.id,
+        },
+      })
+    ).data;
+    this.events = [];
+    for (let i = 0; i < this.resources.length; i += 1) {
+      const item = this.resources[i];
+      this.events.push({
+        name: item.name,
+        start: item.date,
+        id: item.id,
+      });
+    }
+  }
+
+  mounted(): void {
+    this.pullResources();
+    this.$root.$on("resource-refresh-needed", () => {
+      this.pullResources();
+    });
+  }
+}
+</script>
